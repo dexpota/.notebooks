a17e0f989044d28aefc6b360f6efb554ffc41ee6
Fabrizio Destro <7031675+dexpota@users.noreply.github.com>
2019-10-20 23:03:24 +0200

    Update url checking hook

--- .hooks/check-links.sh
+++ .hooks/check-links.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-liche -vr "$1"
+# if liche exits and everything goes fine then exit successfully
+command -v liche 1>/dev/null 2>&1 && liche -vr "$1" && exit $?
+# try with remark, check external links
+command -v remark 1>/dev/null 2>&1 && remark --use lint-no-dead-urls "$1"
+# check relative links
+command -v remark 1>/dev/null 2>&1 && remark --use validate-links "$1"
